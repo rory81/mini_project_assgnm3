7f8ef7aa4c0134aa0a70b68e1865ba708b447c99
Nathalie <nbleijie@gmail.com>
2019-12-14 19:43:47 +0000

    basics are setup like css, flask templates, mongo and heroku

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+# is to connect to the right database (here:taskmanager)
+app.config["MONGO_DBNAME"] = 'taskmanager'
+# optional voor de pymongo version we are using
+app.config["MONGO_URI"] = 'mongodb+srv://rory81:<>@myfirstcluster-nn45a.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+
+# create an instance of PyMongo and then add the app into that
+mongo = PyMongo(app)
+
+# setup connection to the database
+# routing (@) is a string that, when attach it to a URL, will redirect to a particular function in our Flask application
 @app.route('/')
-
-
-def hello():
-    return 'Hello World Again!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
